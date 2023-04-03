6ae4839ebcd3b8a3c1e45fa23ea47f0e3dd17a05
Nam Nguyễn <nguyenphinam2k2@gmail.com>
2023-04-03 12:48:51 +0700

    add doc api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('RunMate API - Group 7 -Mobile Application Development')
+  .setDescription('API Documentation for CRUD Run of RunMate App ')
+  // .setVersion('1.0')
+  .addTag('runs')
+  .build();
+  
+  const options: SwaggerDocumentOptions =  {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+  
+  const document = SwaggerModule.createDocument(app, config, options);
+  
+  
+  
+  SwaggerModule.setup('api', app, document);
+  
+  console.log('alo')
   await app.listen(3000);
 }
 bootstrap();
